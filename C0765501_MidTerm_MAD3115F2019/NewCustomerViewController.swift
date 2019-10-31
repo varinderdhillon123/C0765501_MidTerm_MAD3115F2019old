f91a44221412a45320b8ac34b3380a48544eade5
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-31 16:25:53 -0400

    [varinder] new customer file

---
+++ C0765501_MidTerm_MAD3115F2019/NewCustomerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewCustomerViewController.swift
+//  C0765501_MidTerm_MAD3115F2019
+//
+//  Created by MacStudent on 2019-10-31.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class NewCustomerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
